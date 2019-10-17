9e2355c9a30ad9ee71674085a6769b953670c748
Milton <mhuyn018@ucr.edu>
2019-10-17 15:48:56 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css'
+import Navigation from './Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navigation className="Nav"></Navigation>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navigation.css';
+
+function Navigation() {
+    return (
+        <ul className="Nav">
+            <li><a href="www.google.com">Google</a></li>
+            <li><a href="www.yahoo.com">Yahoo</a></li>
+        </ul>
+    );
+}
+
+export default Navigation;
