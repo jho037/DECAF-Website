6c8811c8528391e37d2c2fd3163ff04c174b1b70
jho037 <jho037@ucr.edu>
2019-10-17 18:38:50 -0700

    Decaf Summary

---
+++ src/DecafSum.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './DecafSum.css';
+
+function DecafSum() {
+    return (
+        <div className="Summary">
+            
+            <h1>DECAF++</h1>
+            <h2>(Dynamic Executable Code Analysis Framework)</h2>
+            
+            <p>DECAF++, the new version of DECAF, taint analysis is around 2X faster making it the fastest,
+            to the best of our knowledge, whole-system dynamic taint analysis framework. This results in 
+            a much better usability imposing only 4% overhead (SPEC CPU2006) when no suspicious (tainted) 
+            input exists. Even under heavy taint analysis workloads, DECAF++ has a much better performance,
+            around 25% faster on nbench, because of its elasticity. DECAF++ elasticity makes it a very suitable
+            case for security analysis tasks that would selectively analyze the input e.g. Intrusion Detection Systems (IDS)
+            that can filter out benign traffic. For further technical details, see our <a href="https://www.cs.ucr.edu/~adava003/RAID__Elastic_Whole_System_Dynamic_Taint_Analysis.pdf">Raid 2019 paper.</a>
+             To activate the optimizations, see our <a href="https://github.com/decaf-project/DECAF/wiki/DECAF--plus-plus">DECAF++ Wiki</a> page.
+            </p>
+
+            <img src="https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg"></img>
+
+        </div>
+    );
+}
+
+export default DecafSum;
